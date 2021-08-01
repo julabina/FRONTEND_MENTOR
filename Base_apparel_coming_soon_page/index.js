cc9207d86cf4f4784060cb273167b4a90d3eaa63
Lenfumé Julien <julabina@hotmail.fr>
2021-08-01 20:42:03 +0200

    01 08 21 2 41

--- Base_apparel_coming_soon_page/index.js
+++ Base_apparel_coming_soon_page/index.js
@@ -1,6 +1,30 @@
 const mail = document.querySelector(".mailInput");
 const btn = document.querySelector(".arrowContainer");
-const span = document.querySelector(".mailSpan > span");
 const error = document.getElementById("error");
 
-const displayError = (message, valid) => {};
+const displayError = (message, valid) => {
+  const span = document.querySelector(".mailSpan > span");
+  const mailBox = document.querySelector(".mail");
+};
+
+const mailCheck = (value) => {
+  if (!value.match(/^[\w_-]+@[\w-]+\.[a-z]{2,4}$/i)) {
+    displayError("");
+    console.log("pas bon");
+  } else {
+    console.log("bon");
+  }
+};
+
+mail.addEventListener("input", (e) => {
+  mailCheck(e.target.value);
+});
+
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (mailCheck) {
+    console.log("popopo");
+  } else {
+    console.log("doingdoingdoing");
+  }
+});
