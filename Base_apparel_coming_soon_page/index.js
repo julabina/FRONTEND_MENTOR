cd206376b12c5ed992ab2e4e1d31e229493788b2
Lenfumé Julien <julabina@hotmail.fr>
2021-08-01 19:48:37 +0200

    01 08 21 19 48

---
+++ Base_apparel_coming_soon_page/index.js
@@ -0,0 +1,6 @@
+const mail = document.querySelector(".mailInput");
+const btn = document.querySelector(".arrowContainer");
+const span = document.querySelector(".mailSpan > span");
+const error = document.getElementById("error");
+
+const displayError = (message, valid) => {};
