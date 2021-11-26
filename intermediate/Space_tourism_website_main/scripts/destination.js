430d564ef6447aa963dbaf91b4cceb2f917d3c34
Lenfumé Julien <julabina@hotmail.fr>
2021-11-26 09:01:30 +0100

    dev destination.js

--- intermediate/Space_tourism_website_main/scripts/destination.js
+++ intermediate/Space_tourism_website_main/scripts/destination.js
@@ -1,7 +1,60 @@
+const pictureM = document.querySelector(".imgContainerDestin");
+const nameM = document.querySelector(".moonName");
+const descriptionM = document.querySelector(".moonDescription");
+const distanceM = document.querySelector(".moonDistance");
+const travelTimeM = document.querySelector(".moonTravelTime");
+const moons = document.querySelectorAll(".moonNav-p");
+
 let datas;
 
 fetch("../data.json")
   .then((res) => res.json())
   .then((data) => {
     datas = data;
+    console.log(datas);
+    displayMoons();
   });
+
+const resetMoonsSelected = () => {
+  for (let i = 0; i < moons.length; i++) {
+    moons[i].classList.remove("moonSelected");
+  }
+};
+
+const displayMoons = () => {
+  for (let i = 0; i < moons.length; i++) {
+    if (moons[i].classList.contains("moonSelected")) {
+      nameM.textContent = datas.destinations[i].name;
+      descriptionM.textContent = datas.destinations[i].description;
+      distanceM.textContent = datas.destinations[i].distance;
+      travelTimeM.textContent = datas.destinations[i].travel;
+      pictureM.innerHTML = `
+      <img src="${datas.destinations[i].images.png}" alt="picture of ${datas.destinations[i].name}">
+      `;
+    }
+  }
+};
+
+moons[0].addEventListener("click", () => {
+  resetMoonsSelected();
+  moons[0].classList.add("moonSelected");
+  displayMoons();
+});
+
+moons[1].addEventListener("click", () => {
+  resetMoonsSelected();
+  moons[1].classList.add("moonSelected");
+  displayMoons();
+});
+
+moons[2].addEventListener("click", () => {
+  resetMoonsSelected();
+  moons[2].classList.add("moonSelected");
+  displayMoons();
+});
+
+moons[3].addEventListener("click", () => {
+  resetMoonsSelected();
+  moons[3].classList.add("moonSelected");
+  displayMoons();
+});
