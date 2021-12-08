2cd14dd98a5acd283513bc5c8fdcafdce2dd751f
Lenfumé Julien <julabina@hotmail.fr>
2021-12-08 15:28:09 +0100

    space websit make responsive header and home for mobile

---
+++ intermediate/Space_tourism_website_main/scripts/header.js
@@ -0,0 +1,17 @@
+const hamburgerBtn = document.querySelector(".menuMobileHamb");
+const crossBtn = document.querySelector(".menuMobileCross");
+const nav = document.querySelector(".navHeader");
+
+hamburgerBtn.addEventListener("click", () => {
+  crossBtn.classList.remove("invisible");
+  hamburgerBtn.classList.remove("visible");
+  hamburgerBtn.classList.add("invisible");
+  nav.classList.remove("invisible");
+});
+
+crossBtn.addEventListener("click", () => {
+  hamburgerBtn.classList.remove("invisible");
+  hamburgerBtn.classList.add("visible");
+  crossBtn.classList.add("invisible");
+  nav.classList.add("invisible");
+});
