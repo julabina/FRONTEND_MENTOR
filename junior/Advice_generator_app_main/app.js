911ef2fb1a250a333359ffa842991567cef690e9
Lenfumé Julien <julabina@hotmail.fr>
2022-02-24 09:25:49 +0100

    update screenshot.webp - Crowfunding page

--- junior/Advice_generator_app_main/app.js
+++ junior/Advice_generator_app_main/app.js
@@ -2,17 +2,19 @@ const adviceId = document.getElementById("adviceId");
 const advice = document.getElementById("advice");
 const diceBtn = document.querySelector(".dice");
 
+
 const newAdvice = () => {
     fetch("https://api.adviceslip.com/advice")
     .then((res) => res.json())
     .then((data) => {
+        console.log(data.slip.id);
         adviceId.textContent = data.slip.id;
         advice.textContent = data.slip.advice;
-    })
+    }) 
 }
 
 diceBtn.addEventListener("click", () => {
-    newAdvice();
+    newAdvice(true);
 })
 
-newAdvice();
+newAdvice(true);
